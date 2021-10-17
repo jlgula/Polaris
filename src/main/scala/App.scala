d2206b6ac7d2c56dcdd2915d3f7d368fe9141c07
James Gula <jlgula@papugh.com>
2021-10-17 15:58:17 -0700

    Fixed get, post, update to use map data.

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,16 +1,18 @@
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration.Duration
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ContentType
-
 import foo.pet._
 import foo.definitions.Pet
+import io.circe.Json
+
 import java.io.File
+import scala.collection.mutable
 
 object App extends App {
+  private val pets = mutable.HashMap[String, Option[String]]()
 
   implicit def actorSystem = ActorSystem()
 
@@ -27,11 +29,24 @@ object App extends App {
 
     // application/json
     def createPet(respond: PetResource.CreatePetResponse.type)(body: Pet): Future[PetResource.CreatePetResponse] = {
+      println("createPet")
+      pets.put(body.name, body.status)
       Future.successful(respond.OK(body))
     }
-    override def updatePet(respond: PetResource.UpdatePetResponse.type)(name: String, body: Option[Pet]): Future[PetResource.UpdatePetResponse] = ???
+
+    override def updatePet(respond: PetResource.UpdatePetResponse.type)(name: String, body: Option[Pet]): Future[PetResource.UpdatePetResponse] = {
+      if (body.isDefined && pets.contains(name)) {
+        body.foreach(pet => pets.update(pet.name, pet.status))
+        Future.successful(respond.OK(body.get))
+      } else Future.successful(respond.BadRequest(Json.obj("message" -> Json.fromString(s"not found: $name"))))
+    }
+
+    override def getPets(respond: PetResource.GetPetsResponse.type)(): Future[PetResource.GetPetsResponse] = {
+      println("getPets")
+      Future.successful(respond.OK(pets.keys.map(key => Pet(key, pets(key))).toVector))
+    }
   })
 
   Await.result(Http().newServerAt("127.0.0.1", 8080).bindFlow(routes), Duration.Inf)
-  println("Running at http://localhost:8080 !")
+  println("Running at http://localhost:8080!")
 }
