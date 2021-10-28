5abd5d8595f2a8104ab09be0decdc0cdce91a063
James Gula <jlgula@papugh.com>
2021-10-27 17:52:31 -0700

    Top level functionality refactored into class for testing.

--- src/main/scala/org/opendcgrid/app/pclient/PolarisClient.scala
+++ src/main/scala/org/opendcgrid/app/pclient/PolarisClient.scala
@@ -3,25 +3,36 @@ package org.opendcgrid.app.pclient
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import org.opendcgrid.app.pclient.definitions.Device
 import org.opendcgrid.app.pclient.device.{DeviceClient, ListDevicesResponse}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success, Try}
 
 object PolarisClient extends App {
+  val defaultHost = "http://localhost:8080"
+  val client = new PolarisClient(defaultHost)
+  println(client.listDevices())
+  client.terminate()
+}
+
+class PolarisClient(val host: String) {
   implicit val system: ActorSystem = ActorSystem()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   implicit val requester: HttpRequest => Future[HttpResponse] = Http().singleRequest(_)
-  val deviceClient = DeviceClient()
-  val result = deviceClient.listDevices()
+  private val deviceClient = DeviceClient(host)
 
-  Await.result(result.value, Duration.Inf) match {
-    case Right(ListDevicesResponse.OK(value)) => println(value)
-    case Right(ListDevicesResponse.BadRequest) => println(s"Bad request.")
-    case Left(Left(throwable)) => println(s"Failed: ${throwable.getMessage}")
-    case Left(Right(response)) => println(s"Failed: unexpected response $response")
+  def listDevices(): Try[Vector[Device]] = {
+    val result = deviceClient.listDevices()
+
+    Await.result(result.value, Duration.Inf) match {
+      case Right(ListDevicesResponse.OK(value)) => Success(value)
+      case Right(ListDevicesResponse.BadRequest) => Failure(new IllegalStateException(s"Bad request."))
+      case Left(Left(throwable)) => Failure(throwable)
+      case Left(Right(response)) => Failure(new IllegalStateException(s"Failed: unexpected response $response"))
+    }
   }
 
-  Await.result(system.terminate(), Duration.Inf)
-  //println("done")
+  def terminate(): Unit = Await.result(system.terminate(), Duration.Inf)
 }
