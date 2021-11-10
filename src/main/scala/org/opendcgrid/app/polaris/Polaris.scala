0e2c36ceb6e69f0d178958bfa1b918e29b3d0260
James Gula <jlgula@papugh.com>
2021-11-09 18:07:39 -0800

    Minor refactor of Polaris server code into separate class.

--- src/main/scala/org/opendcgrid/app/polaris/Polaris.scala
+++ src/main/scala/org/opendcgrid/app/polaris/Polaris.scala
@@ -1,27 +1,34 @@
 package org.opendcgrid.app.polaris
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
-import akka.http.scaladsl.server.Directives._
-import org.opendcgrid.app.polaris.device.{DeviceResource, PolarisDeviceHandler}
-import org.opendcgrid.app.polaris.gc.{GcResource, PolarisGCHandler}
-import org.opendcgrid.app.polaris.subscription.{PolarisSubscriptionHandler, SubscriptionResource}
+import akka.http.scaladsl.model.Uri
 
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.concurrent.duration.Duration
+import java.io.{BufferedReader, PrintStream}
 
 object Polaris extends App {
-  implicit def actorSystem: ActorSystem = ActorSystem()
-  implicit val context: ExecutionContext = actorSystem.dispatcher
-  implicit val requester: HttpRequest => Future[HttpResponse] = Http().singleRequest(_)
-  private val subscriptionHandler = new PolarisSubscriptionHandler
-  private val subscriptionRoutes = SubscriptionResource.routes(subscriptionHandler)
-  private val url = Uri("http://localhost:8080")
-  private val deviceHandler = new PolarisDeviceHandler(url, subscriptionHandler)
-  private val deviceRoutes = DeviceResource.routes(deviceHandler)
-  private val gcRoutes = GcResource.routes(new PolarisGCHandler(deviceHandler, subscriptionHandler))
-  private val routes = deviceRoutes ~ gcRoutes ~ subscriptionRoutes
-  Await.result(Http().newServerAt(url.authority.host.toString(), url.authority.port).bindFlow(routes), Duration.Inf)
-  println("Running at http://localhost:8080!")
+  val app = new Polaris()
+  val result = app.run(this.args.toIndexedSeq, Console.in, Console.out, Console.err)
+  System.exit(result)
+}
+
+class Polaris {
+
+  /**
+   * Runs the application.
+   *
+   * @param args command line arguments that are parsed and passed to the application
+   * @param in   [[BufferedReader]] that provides input
+   * @param out  [[PrintStream]] used to display normal output
+   * @param err  [[PrintStream]] used to display error messages
+   * @return The exit code for the application. An exit code of 0 means normal exit. Non zero values mean various errors. See TBD for specifics.
+   */
+  def run(args: Seq[String], in: BufferedReader, out: PrintStream, err: PrintStream): Int = {
+    val url = Uri("http://localhost:8080")
+    implicit def actorSystem: ActorSystem = ActorSystem()
+    val server = new PolarisServer(url)
+    server.run()
+    0
+  }
+
+
 }
---
+++ src/main/scala/org/opendcgrid/app/polaris/PolarisServer.scala
@@ -0,0 +1,32 @@
+package org.opendcgrid.app.polaris
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.http.scaladsl.server.Directives._
+import org.opendcgrid.app.polaris.device.{DeviceResource, PolarisDeviceHandler}
+import org.opendcgrid.app.polaris.gc.{GcResource, PolarisGCHandler}
+import org.opendcgrid.app.polaris.subscription.{PolarisSubscriptionHandler, SubscriptionResource}
+
+import java.util.concurrent.Semaphore
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+class PolarisServer(uri: Uri) (implicit actorSystem: ActorSystem) extends Runnable {
+  private val haltSemaphore = new Semaphore(0)
+  override def run(): Unit = {
+    implicit val context: ExecutionContext = actorSystem.dispatcher
+    implicit val requester: HttpRequest => Future[HttpResponse] = Http().singleRequest(_)
+    val subscriptionHandler = new PolarisSubscriptionHandler
+    val subscriptionRoutes = SubscriptionResource.routes(subscriptionHandler)
+    val deviceHandler = new PolarisDeviceHandler(uri, subscriptionHandler)
+    val deviceRoutes = DeviceResource.routes(deviceHandler)
+    val gcRoutes = GcResource.routes(new PolarisGCHandler(deviceHandler, subscriptionHandler))
+    val routes = deviceRoutes ~ gcRoutes ~ subscriptionRoutes
+    Await.result(Http().newServerAt(uri.authority.host.toString(), uri.authority.port).bindFlow(routes), Duration.Inf)
+    println(s"Running at ${uri.toString()}!")
+    haltSemaphore.acquire()
+  }
+
+  def halt(): Unit = haltSemaphore.release()
+}
