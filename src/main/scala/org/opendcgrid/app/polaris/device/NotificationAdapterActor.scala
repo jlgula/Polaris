7b1623e6abb6429dc504b8e27412fce9102b54fc
James Gula <jlgula@papugh.com>
2022-01-05 17:41:58 -0800

    Initial implementation of NotificationAdapterActor.

---
+++ src/main/scala/org/opendcgrid/app/polaris/device/NotificationAdapterActor.scala
@@ -0,0 +1,61 @@
+package org.opendcgrid.app.polaris.device
+
+import akka.Done
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.pattern.StatusReply
+import org.opendcgrid.app.polaris.server.definitions.Notification
+import org.opendcgrid.app.polaris.server.notification.{NotificationClient, PostNotificationResponse}
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
+object NotificationAdapterActor {
+  sealed trait Command
+  final case class WrappedNotification(notification: NotificationProtocol.Notification, replyTo: ActorRef[StatusReply[Done]]) extends Command
+  final case object TransmissionSuccessful extends Command
+  final case class TransmissionFailed(error: DeviceError) extends Command
+  def apply(client: NotificationClient): Behavior[Command] = Behaviors.setup[Command](context => new NotificationAdapterActor(context, client))
+}
+
+/**
+ * Forwards a notification to an HTTP client and sends a reply when it hears from the client.
+ *
+ * @param context the [[ActorContext]] for this actor
+ * @param client the generated client to send to the target
+ */
+class NotificationAdapterActor(context: ActorContext[NotificationAdapterActor.Command], client: NotificationClient)  extends AbstractBehavior[NotificationAdapterActor.Command](context) {
+  import NotificationAdapterActor._
+  implicit private val ec: ExecutionContext = context.executionContext
+  var replyTo: Option[ActorRef[StatusReply[Done]]] = None
+  /*
+  val notificationAdapter: ActorRef[NotificationProtocol.Notify] = context.messageAdapter[NotificationProtocol.Notify] {
+    case NotificationProtocol.Notify(notification, replyTo) => WrappedNotification(notification, replyTo)
+  }
+
+   */
+
+  override def onMessage(msg: Command): Behavior[Command] = msg match {
+    case WrappedNotification(notification, replyTo) =>
+      this.replyTo = Some(replyTo)
+      val serializedNotification: Notification = Notification(notification.observed.toString(), notification.action.toString, notification.value)
+      val futureResult = client.postNotification(serializedNotification).value
+      context.pipeToSelf(futureResult) {
+        // map the Future value to a message, handled by this actor
+        case Success(result) => result match {
+          case Right(PostNotificationResponse.NoContent) => TransmissionSuccessful
+          case Right(PostNotificationResponse.BadRequest(details)) => TransmissionFailed(DeviceError.InvalidNotification(serializedNotification, details))
+          case Left(Left(error)) => TransmissionFailed(DeviceError.NotificationFailed(serializedNotification, error.getMessage))
+          case Left(Right(httpResponse)) => TransmissionFailed(DeviceError.NotificationFailed(serializedNotification, httpResponse.toString()))
+        }
+        case Failure(e) =>TransmissionFailed(DeviceError.NotificationFailed(serializedNotification, e.toString))
+      }
+      Behaviors.same
+    case TransmissionSuccessful =>
+      this.replyTo.get ! StatusReply.ack()
+      Behaviors.same
+    case TransmissionFailed(error) =>
+      this.replyTo.get ! StatusReply.error(error)
+      Behaviors.same
+  }
+}
---
+++ src/test/scala/org/opendcgrid/app/polaris/device/NotificationAdapterActorTest.scala
@@ -0,0 +1,49 @@
+package org.opendcgrid.app.polaris.device
+
+import akka.Done
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.http.scaladsl.server.Route
+import akka.pattern.StatusReply
+import akka.stream.Materializer
+import org.opendcgrid.app.polaris.client.notification.NotificationResource
+import org.opendcgrid.app.polaris.command.CommandUtilities
+import org.opendcgrid.app.polaris.server.notification.NotificationClient
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class NotificationAdapterActorTest extends ScalaTestWithActorTestKit with AnyFunSuiteLike {
+  implicit val sys: ActorSystem[Nothing] = system
+  test("notification") {
+    val fixture = new NotificationAdapterFixture
+    val probe = createTestProbe[StatusReply[Done]]()
+
+    val adapterActor = spawn(NotificationAdapterActor(fixture.notificationClient))
+    val notification = NotificationProtocol.Notification(fixture.path, fixture.action, fixture.value)
+    adapterActor ! NotificationAdapterActor.WrappedNotification(notification, probe.ref)
+    val response = probe.receiveMessage()
+    assert(response.isSuccess)
+  }
+}
+
+class NotificationAdapterFixture(implicit system: ActorSystem[Nothing]) {
+  implicit val requester: HttpRequest => Future[HttpResponse] = Http().singleRequest(_)
+  implicit def context: ExecutionContext = system.executionContext
+  private val localHost = Uri("http://localhost")
+  private val gcPort = CommandUtilities.getUnusedPort
+  //private val observerPort = CommandUtilities.getUnusedPort
+  private val gcURI = localHost.withPort(gcPort)
+  val testHandler = new TestNotificationHandler
+  private val notificationRoutes = NotificationResource.routes(testHandler)
+  val deviceID = "123"
+  private val routeFunction: HttpRequest => Future[HttpResponse] = Route.toFunction(notificationRoutes)
+  private val materializer = Materializer(system)
+  val notificationClient: NotificationClient = NotificationClient(gcURI.toString())(routeFunction, context, materializer)
+  val path: Uri.Path = gcURI.path
+  val action: NotificationAction = NotificationAction.Put
+  val value = "1.0"
+}
+
